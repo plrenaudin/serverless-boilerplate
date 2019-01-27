43d2bf3c25a629f824e2f2e6ffa119ccdc1c200e
plrenaudin <pl.renaudin@gmail.com>
2019-01-27 10:05:11 -0500

    add eslint + prettier

--- components/clock.js
+++ components/clock.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "isomorphic-unfetch";
 
 const Clock = ({ now }) => (
--- devServer.js
+++ devServer.js
@@ -1,21 +1,24 @@
+/* eslint-disable */
 const express = require("express");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = true;
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+/**
+ * Use this only on dev mode for running lambdas locally without having to deploy to now
+ */
 
 app
   .prepare()
   .then(() => {
     const server = express();
 
-    if (dev) {
-      // run lambdas locally
-      server.get("/api/:service", (req, res) => {
-        require(`./api/${req.params.service}`)(req, res);
-      });
-    }
+    // run lambdas locally
+    server.get("/api/:service", (req, res) => {
+      require(`./api/${req.params.service}`)(req, res);
+    });
 
     server.get("*", (req, res) => {
       return handle(req, res);
