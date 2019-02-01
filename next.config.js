324a582c4ee263b73c25f29b1bdf62a75524e094
plrenaudin <pl.renaudin@gmail.com>
2019-01-31 23:38:59 -0500

    workaround next config issue Will be fixed in Next 8

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /* eslint-disable */
-const withLess = require("@zeit/next-less");
-
-module.exports = withLess({
+const nextConfig = {
   cssModules: true,
   target: "serverless"
-});
+};
+
+const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === "development" ? {} : require("next-server/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return nextConfig;
+  }
+  const withLess = require("@zeit/next-less");
+  return withLess(nextConfig);
+};
