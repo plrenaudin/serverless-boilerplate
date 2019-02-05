a1ebe21ac3f098a44d6fa9a2bbe7d2121e1a9dc5
plrenaudin <pl.renaudin@gmail.com>
2019-02-04 20:48:28 -0500

    fix next,js config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
   target: "serverless"
 };
 
-const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === "development" ? {} : require("next-server/constants");
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : !process.env.NOW_REGION
+    ? require("next/constants")
+    : require("next-server/constants");
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
