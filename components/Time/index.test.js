48acabf9f9039ddd6c2c753f5d9585ed5e930a82
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2019-08-25 22:58:26 +0100

    remove Time component

--- components/Time/index.test.js
+++
@@ -1,7 +0,0 @@
-import Time from ".";
-import renderer from "react-test-renderer";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<Time now={1548726138665} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
